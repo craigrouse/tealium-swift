6c41085961963df68d339145e69139915fe37f90
craigrouse <craig.rouse@gmail.com>
2019-06-17 10:58:43 +0100

    anycodable v1

---
+++ tealium/diskstorage/TealiumDiskStorageConstants.swift
@@ -0,0 +1,10 @@
+//
+// Created by Craig Rouse on 2019-06-14.
+// Copyright (c) 2019 Tealium, Inc. All rights reserved.
+//
+
+import Foundation
+
+enum TealiumDiskStorageConstants: String {
+    case moduleName = "diskstorage"
+}
---
+++ tealium/diskstorage/TealiumDiskStorageModule.swift
@@ -0,0 +1,64 @@
+//
+// Created by Craig Rouse on 2019-06-14.
+// Copyright (c) 2019 Tealium, Inc. All rights reserved.
+//
+
+import Foundation
+import TealiumCore
+
+class TealiumDiskStorageModule: TealiumModule {
+
+    let defaultDirectory = Disk.Directory.documents
+
+    override class func moduleConfig() -> TealiumModuleConfig {
+        return  TealiumModuleConfig(name: TealiumDiskStorageConstants.moduleName.rawValue,
+                                    priority: 350,
+                                    build: 1,
+                                    enabled: true)
+    }
+
+    override func handle(_ request: TealiumRequest) {
+
+        if let request = request as? TealiumEnableRequest {
+            enable(request)
+        } else if let request = request as? TealiumDisableRequest {
+            disable(request)
+        } else if let request = request as? TealiumLoadRequest {
+            load(request)
+        } else if let request = request as? TealiumSaveRequest {
+            save(request)
+        } else if let request = request as? TealiumDeleteRequest {
+//            delete(request)
+        } else {
+            didFinishWithNoResponse(request)
+        }
+    }
+
+    override func enable(_ request: TealiumEnableRequest) {
+
+        isEnabled = true
+//        filenamePrefix = TealiumFileStorageModule.filenamePrefix(config: request.config)
+        didFinish(request)
+    }
+
+    func load(_ request: TealiumLoadRequest) {
+
+        let requestingModule = request.name
+
+//        Disk.retrieve("posts.json", from: .documents, as: [Post].self)
+        guard let storedData = try? Disk.retrieve(requestingModule, from: defaultDirectory, as: [String: AnyCodable].self) else {
+            request.completion?(true, nil, nil)
+            didFinishWithNoResponse(request)
+            return
+        }
+        request.completion?(true, storedData, nil)
+    }
+
+    func save(_ request: TealiumSaveRequest) {
+        let requestingModule = request.name
+
+        try? Disk.save(request.data.codable, to: defaultDirectory, as: requestingModule)
+
+    }
+
+}
